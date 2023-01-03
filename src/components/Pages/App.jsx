803dc2ba0dcea08b287812b95a545a0b649cb500
hector <hectorrrivera1010@gmail.com>
2023-01-02 21:48:22 -0600

    first section finished

--- src/components/Pages/App.jsx
+++ src/components/Pages/App.jsx
@@ -1,16 +1,25 @@
-import Spline from '@splinetool/react-spline'
-import '../Pages/App.css';
-import Nav from '../Navigation/Nav'
+import Spline from "@splinetool/react-spline";
+import "../Pages/App.css";
+import Nav from "../Navigation/Nav";
 
 function App() {
-
   return (
     <div className="App">
-      <Spline scene="https://prod.spline.design/SmPf8eobVcIKS97G/scene.splinecode" height={window.innerHeight}/>
-      <h1 className='text'>Explore the wonders of our home planet with us</h1>
-      <Nav/>
+      <Spline
+        scene="https://prod.spline.design/SmPf8eobVcIKS97G/scene.splinecode"
+        height={window.innerHeight}
+      />
+      <Nav className="Nav" />
+      <div className="text">
+        <h1>Explore the wonders of our home planet with us</h1>
+        <p>
+          learn and discover the many amazing features of planet Earth. From the
+          highest mountains to the depths of the oceans, our home planet is full
+          of wonders waiting to be explored.
+        </p>
+      </div>
     </div>
-  )
-} 
+  );
+}
 
-export default App
+export default App;
